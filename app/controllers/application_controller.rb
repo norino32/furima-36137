d97c1c325e3660d1b14c35b1fce26610b3def83a
norino32 <masanori.sasahara1990h@gmail.com>
2021-08-10 18:20:01 +0900

    修正３

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'sasahara' && password == '0327'
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
